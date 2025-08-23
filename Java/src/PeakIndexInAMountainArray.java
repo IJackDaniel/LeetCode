73cb9b23b0a9a0df51c7d0a7a17ab2c767724dae
Danya <danya05af@gmail.com>
2025-08-23 16:17:15 +0300

    Edit File Creator

---
+++ Java/src/PeakIndexInAMountainArray.java
@@ -0,0 +1,23 @@
+// Task name: 852. Peak Index in a Mountain Array
+// Difficulty: Medium
+// Condition: Task has not been solved
+// Date of creation: 2025-08-23
+
+public class PeakIndexInAMountainArray {
+    public static void main(String[] args) {
+        var result = peakIndexInMountainArray(new int[] {0,1,0});
+        System.out.println(result);
+        result = peakIndexInMountainArray(new int[] {0,2,1,0});
+        System.out.println(result);
+        result = peakIndexInMountainArray(new int[] {0,10,5,2});
+        System.out.println(result);
+//        result = peakIndexInMountainArray(new int[] {});
+//        System.out.println(result);
+//        result = peakIndexInMountainArray(new int[] {});
+//        System.out.println(result);
+    }
+
+    static int peakIndexInMountainArray(int[] arr) {
+        return 0;
+    }
+}
