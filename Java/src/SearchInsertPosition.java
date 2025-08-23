1275c3d2434807e1c0c45fa7d85c33fe1fd7daa9
Danya <danya05af@gmail.com>
2025-08-23 17:04:09 +0300

    Solve task '35. Search Insert Position' the best way

--- Java/src/SearchInsertPosition.java
+++ Java/src/SearchInsertPosition.java
@@ -1,6 +1,6 @@
 // Task name: 35. Search Insert Position
 // Difficulty: Easy
-// Condition: Task has not been solved
+// Condition: Task has been solved
 // Date of creation: 2025-08-23
 
 public class SearchInsertPosition {
@@ -11,13 +11,23 @@ public class SearchInsertPosition {
         System.out.println(result);
         result = searchInsert(new int[] {1,3,5,6}, 7);
         System.out.println(result);
-//        result = searchInsert(new int[] {}, 0);
-//        System.out.println(result);
+        result = searchInsert(new int[] {1, 3}, 2);
+        System.out.println(result);
 //        result = searchInsert(new int[] {}, 0);
 //        System.out.println(result);
     }
 
     static int searchInsert(int[] nums, int target) {
-        return 0;
+        if (target < nums[0]) return 0;
+        if (target > nums[nums.length-1]) return nums.length;
+        int left = 0, right = nums.length - 1 ;
+        while (right - left >= 1) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target && nums[mid-1] < target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
     }
 }
