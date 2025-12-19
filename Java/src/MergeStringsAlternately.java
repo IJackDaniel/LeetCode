3121217c0336752031e626b58765241715ff38b6
Danya <danya05af@gmail.com>
2025-12-19 17:37:51 +0300

    Optimize task 1768

--- Java/src/MergeStringsAlternately.java
+++ Java/src/MergeStringsAlternately.java
@@ -14,7 +14,7 @@ public class MergeStringsAlternately {
     static String mergeAlternately(String word1, String word2) {
         int i = 0;
         int n = Math.max(word1.length(), word2.length());
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder(word1.length() + word2.length());
         while (i < n) {
             if (i < word1.length()) {
                 result.append(word1.charAt(i));
